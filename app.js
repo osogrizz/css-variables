276ac9c9a23a8794e43175adaad4bf49a2cce0b7
Leo Torres <leo.htor@gmail.com>
2018-04-02 18:06:45 -0400

    Added event listeners to inputs

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+inputs = document.querySelectorAll('.controls input');
 
+function handleUpdate() {
+  const suffix = this.dataset.suffix || '';
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
